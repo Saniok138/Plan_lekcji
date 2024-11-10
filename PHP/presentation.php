f0b22e5243d9b97b5da906c6d66654001773bc8e
inorf <dimond23177@gmail.com>
2024-11-10 18:29:25 +0300

    sum

---
+++ PHP/presentation.php
@@ -0,0 +1,4 @@
+<?php
+    $selected_option = $_POST['change-classes'];
+    include("../classes/$selected_option.php");
+?>
--- index.php
+++ index.php
@@ -1,3 +1,21 @@
-<?php 
-
-?>
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>GENERATOR</title>
+    <link rel="stylesheet" href="./CSS/main-style.css">
+</head>
+<body>
+    <div class="menu-main">
+        <?php 
+            if(!isset($_COOKIE["generated"])){
+                setcookie("generated", "TRUE", time() + (86400 * 365),"/");
+                include('./HTML/menu-generation.html');
+            }else{
+                include('./HTML/menu-presentation.html');
+            }
+        ?>
+    </div>
+</body>
+</html>
