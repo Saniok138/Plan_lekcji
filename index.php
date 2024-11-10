74259fa6efb00972f1b957deb9c3d152d2056df9
inorf <dimond23177@gmail.com>
2024-11-10 19:01:37 +0300

    sum

--- index.php
+++ index.php
@@ -4,7 +4,6 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>GENERATOR</title>
-    <link rel="stylesheet" href="./CSS/main-style.css">
 </head>
 <body>
     <div class="menu-main">
