dc3181604f638439c2b99f0f9a769102a89ad302
Saniok138 <112718886+Saniok138@users.noreply.github.com>
2024-11-11 18:06:56 +0100

    Widoki bilyi

---
+++ other/zadanie_generowanie_Bilyi/pierwsza_godzina_n.sql
@@ -0,0 +1,41 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Nov 11, 2024 at 06:06 PM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `plan_lekcji`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure for view `pierwsza_godzina_n`
+--
+
+CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW `pierwsza_godzina_n`  AS SELECT `g`.`id_g` AS `godzina`, `n`.`imie_nazwisko` AS `nauczyciel`, `p`.`nazwa` AS `przedmiot`, `pl`.`numer_s` AS `sala` FROM (((`godzina` `g` join `plan_lekcji` `pl` on(`g`.`id_g` = `pl`.`id_g`)) join `nauczyciele` `n` on(`n`.`skrot` = `pl`.`skrot`)) join `przedmiot` `p` on(`pl`.`id_p` = `p`.`id_p`)) WHERE `g`.`id_g` = 1 ;
+
+--
+-- VIEW `pierwsza_godzina_n`
+-- Data: None
+--
+
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
