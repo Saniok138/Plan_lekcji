19e6cf7016699737af11a378df062a4093b3444b
inorf <dimond23177@gmail.com>
2024-11-21 00:06:22 +0300

    changes in admin.php, and all of 2 .css files
    had added <select>, linked css and done some work on responibility in admin.php. had created new css file admin-style.css for admin.php, done changes in main-style.css.

--- admin.php
+++ admin.php
@@ -1,13 +1,21 @@
-<style>
-  body{
-    position: block;
-    background: linear-gradient(45deg, #b64c4c, #bb4e74, #845ec2, #2a9d8f);
-    background-size: 350% 350%;
-    }
-    table{
-      background: white;
-    }
-</style>
+<link rel="stylesheet" href="./CSS/admin-style.css">
+<body class="background">
+<div class="menu-main">
+<form action='' method="post">
+        <select class="change-classes"  id="change-classes" name="change-classes">
+          <option value="dni_wolne">dni wolne</option>
+          <option value="nauczyciele_klasa">nauczyciele klasa</option>
+          <option value="dni_nauczyciele">dni nauczyciele</option>
+          <option value="nauczyciele_przedmiot">nauczyciele przedmiot</option>
+          <option value="przedmiot_klasa">przedmiot klasa</option>
+          <option value="godzina">godzina</option>
+          <option value="sala">sala</option>
+          <option value="przedmiot">przedmiot</option>
+          <option value="nauczyciele">nauczyciele</option>
+          <option value="klasa">klasa</option>
+        </select>
+        <input type="submit" class="presentation" value="PRESENTATION">
+    </form>
 <?php 
 session_start();
 if(!isset($_SESSION["Admin"])){
@@ -23,39 +31,91 @@ if(!isset($_SESSION["Admin"])){
   die("Error connect: " . mysqli_connect_error());
   }
 
-  echo "<h3>dni_wolne</h3>";
-  include("./admin/dni_wolne.php");
+  $option = $_POST["change-classes"] ?? null;
 
-  echo "<br><br><h3>nauczyciele_klasa</h3>";
-  include("./admin/nauczyciele_klasa.php");
+  if( $option == "dni_wolne"){
+    echo "<h3 style='color: antiquewhite;'>dni_wolne</h3>";
+    include("./admin/dni_wolne.php");
+  }
 
-  echo "<br><br><h3>dni_nauczyciele</h3>";
-  include("./admin/dni_nauczyciele.php");
+  if($option == "nauczyciele_klasa"){
+    echo "<h3 style='color: antiquewhite;'>nauczyciele_klasa</h3>";
+    include("./admin/nauczyciele_klasa.php");
+  }
 
-  echo "<br><br><h3>nauczyciele_przedmiot</h3>";
-  include("./admin/nauczyciele_przedmiot.php");
+  if($option == "dni_nauczyciele"){
+    echo "<h3 style='color: antiquewhite;'>dni_nauczyciele</h3>";
+    include("./admin/dni_nauczyciele.php");
+  }
 
-  echo "<br><br><h3>przedmiot_klasa</h3>";
-  include("./admin/przedmiot_klasa.php");
+  if($option == "nauczyciele_przedmiot"){
+    echo "<h3 style='color: antiquewhite;'>nauczyciele_przedmiot</h3>";
+    include("./admin/nauczyciele_przedmiot.php");
+  }
 
-  echo "<br><br><h3>godzina</h3>";
-  include("./admin/godzina.php");
+  if($option == "przedmiot_klasa"){
+    echo "<h3 style='color: antiquewhite;'>przedmiot_klasa</h3>";
+    include("./admin/przedmiot_klasa.php");
+  }  
 
-  echo "<br><br><h3>klasa</h3>";
-  include("./admin/klasa.php");
+  if($option == "godzina"){
+    echo "<h3 style='color: antiquewhite;'>godzina</h3>";
+    include("./admin/godzina.php");
+  }  
 
-  echo "<br><br><h3>nauczyciele</h3>";
-  include("./admin/nauczyciele.php");
+  if($option == "klasa"){
+    echo "<h3 style='color: antiquewhite;'>klasa</h3>";
+    include("./admin/klasa.php");
+  }
 
-  echo "<br><br><h3>przedmiot</h3>";
-  include("./admin/przedmiot.php");
+  if($option == "nauczyciele"){
+    echo "<h3 style='color: antiquewhite;'>nauczyciele</h3>";
+    include("./admin/nauczyciele.php");
+  }
 
-  echo "<br><br><h3>sala</h3>";
-  include("./admin/sala.php");
+  if($option == "przedmiot"){
+    echo "<h3 style='color: antiquewhite;'>przedmiot</h3>";
+    include("./admin/przedmiot.php");
+  }
 
-  echo "<br><br><h3>Update</h3>";
+  if($option == "sala"){  
+    echo "<h3 style='color: antiquewhite;'>sala</h3>";
+    include("./admin/sala.php");
+  }
+  // echo "<h3>dni_wolne</h3>";
+  // include("./admin/dni_wolne.php");
+
+  // echo "<br><br><h3>nauczyciele_klasa</h3>";
+  // include("./admin/nauczyciele_klasa.php");
+
+  // echo "<br><br><h3>dni_nauczyciele</h3>";
+  // include("./admin/dni_nauczyciele.php");
+
+  // echo "<br><br><h3>nauczyciele_przedmiot</h3>";
+  // include("./admin/nauczyciele_przedmiot.php");
+
+  // echo "<br><br><h3>przedmiot_klasa</h3>";
+  // include("./admin/przedmiot_klasa.php");
+
+  // echo "<br><br><h3>godzina</h3>";
+  // include("./admin/godzina.php");
+
+  // echo "<br><br><h3>klasa</h3>";
+  // include("./admin/klasa.php");
+
+  // echo "<br><br><h3>nauczyciele</h3>";
+  // include("./admin/nauczyciele.php");
+
+  // echo "<br><br><h3>przedmiot</h3>";
+  // include("./admin/przedmiot.php");
+
+  // echo "<br><br><h3>sala</h3>";
+  // include("./admin/sala.php");
+
+  
   echo '<form action="admin/update.php" method="post">
-  <select name="tables">
+  <h3 style="color: antiquewhite;">Update</h3>
+  <select class="change-classes" name="tables">
   <option value="k">klasa</option>
   <option value="n">nauczyciele</option>
   <option value="p">przedmiot</option>
@@ -67,11 +127,12 @@ if(!isset($_SESSION["Admin"])){
   <option value="dw">dni_wolne</option>
   <option value="dn">dni_nauczyciele</option>
   </select>
-  <input type="submit" name="submit" value="update">
+  <input type="submit" class="presentation" name="submit" value="update">
   </form>';
 
   echo'<br><br><form action="./index.php" method="post">
-      <input type="submit" name="submit" value="return">
+      <input type="submit" class="return" name="submit" value="return">
   </form>';
 }
-?>
+?></div>
+</body>
