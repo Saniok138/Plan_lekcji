af8c2219f6f8871f97cb348db41dfc9aa13efa0e
Saniok138 <112718886+Saniok138@users.noreply.github.com>
2024-11-29 11:37:44 +0100

    select final

--- PHP/menu-presentation.php
+++ PHP/menu-presentation.php
@@ -4,7 +4,7 @@
 </head>
 <body>
     <form action='./PHP/presentation.php' method="post">
-        <select class="change-classes" id="change-classes" name="change-classes select-animation" onfocus="size=2;" onblur="size=1;" onchange="size=1;">
+        <select class="change-classes" id="change-classes" name="change-classes" onfocus="size=2;" onblur="size=1;" onchange="size=1; blur();">
             <script>
                 const classes = [];
                 const all_classes = "1B;1C;1D;1E;1F;1O;1P;2B;2C;2D;2E;2F;2G;2K;2O;2P;3B;3C;3D;3E;3F;3G;3K;3P;3O;4C;4D;4E;4F;4G;4K;4P;4O;5C;5D;5E;5F;5G;5K;5O;5P";
